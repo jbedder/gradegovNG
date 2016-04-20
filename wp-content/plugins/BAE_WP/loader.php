fb69a37b5664f70ed4599e447a6bf923cc5d29de
jbedder <jbedder@gmail.com>
2016-04-19 20:32:24 -0500

    Arbitrary object saving to db
    Not as elegant as I'd hoped. Highly reusable.

--- wp-content/plugins/BAE_WP/loader.php
+++ wp-content/plugins/BAE_WP/loader.php
@@ -11,6 +11,7 @@ class BAE_Base {
 	
 	 BAE_Base function load($id=null, $field=null){
 		 //$field must refer to a primary key.
+		 $this->primary_key=$field;
 		 if(!($query=$db->query("SELECT * FROM ".get_class($this)." Where ".$field."=".$id))){
 			echo "Error creating ".get_class($this).": ".$db->connect_errno;
 		 }
@@ -22,7 +23,20 @@ class BAE_Base {
 		 }
 		return $this;
 	 }
-	 
+	 BAE_Base function save(){
+		  $primary_key=$this->primary_key;
+		  unset($this->primary_key);
+		  $vars=get_object_vars($this);
+		  $selector="";
+		  $primary_value=array_search($primary_key_value,array_flip($vars));
+		  for each($vars as $key=>$value){
+		    $selector.=$key."=\'".$value."\' ,";
+		  }
+		  $selector=rtrim($selector,",");
+		  if(!($query=$db->query("UPDATE  ".get_class($this)." Where ".$primary_key."=".$primary_key_value))){
+			echo "Error Updating ".get_class($this).": ".$db->connect_errno;
+		 }
+	 }
 	function __construct($id=null,$field=null){
 		$this->load($id, $field);	
 	}
